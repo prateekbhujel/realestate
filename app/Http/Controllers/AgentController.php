8c829d12fb961d6dcc12e74a0ae7e2ab6a0f5b24
Pratik Bhujel <prateekbhujelpb@gmail.com>
2023-07-29 15:30:06 +0545

    Agent side Login/Register/Logout/Password Change Completed.

--- app/Http/Controllers/AgentController.php
+++ app/Http/Controllers/AgentController.php
@@ -19,7 +19,7 @@ class AgentController extends Controller
     */
     public function AgentController()
     {
-        return view('agent.agent_dashboard');
+        return view('agent.index');
     }
      /** End of AgentDashboard method. */
 
@@ -60,5 +60,128 @@ class AgentController extends Controller
      }
      /** End of AgentRegister method. */
 
+
+     /** Start of AgentLogout method. 
+      * Logs the user out from the session and sends toaster message.
+      * Destroy the session.
+     */
+     public function AgentLogout(Request $request){
+      Auth::guard('web')->logout();
+
+      $request->session()->invalidate();
+
+      $request->session()->regenerateToken();
+
+       $notification = array(
+          'message' => 'Agent Logout Successfully',
+          'alert-type' => 'success'
+      ); 
+
+      return redirect('/agent/login')->with($notification);
+  }
+     /** End of AgentRegister method. */
+
+
+     /** Start of AgentProfile method. 
+      * Returns the view to the Profile Update.
+     */
+    public function AgentProfile(){
+
+      $id = Auth::user()->id;
+      $profileData = User::find($id);
+      return view('agent.agent_profile',compact('profileData'));
+
+   }
+     /** End of AgentProfile method. */
+
+
+
+     /** Start of AgentProfileStore method. 
+      * Store the Image and Data to db and file.
+     */
+    public function AgentProfileStore(Request $request){
+
+      $id = Auth::user()->id;
+      $data = User::find($id);
+      $data->username = $request->username;
+      $data->name = $request->name;
+      $data->email = $request->email;
+      $data->phone = $request->phone;
+      $data->address = $request->address; 
+
+      if ($request->file('photo')) {
+          $file = $request->file('photo');
+          @unlink(public_path('upload/agent_images/'.$data->photo));
+          $filename = date('YmdHi').$file->getClientOriginalName(); 
+          $file->move(public_path('upload/agent_images'),$filename);
+          $data['photo'] = $filename;  
+      }
+
+      $data->save();
+
+      $notification = array(
+          'message' => 'Agent Profile Updated Successfully',
+          'alert-type' => 'success'
+      );
+
+      return redirect()->back()->with($notification);
+
+   }
+     /** End of AgentProfileStore method. */
+
+
+
+     /** Start of AgentChangePassword method. 
+      * Returns the view for password Change
+     */
+      public function AgentChangePassword(){
+     
+             $id = Auth::user()->id;
+             $profileData = User::find($id);
+             return view('agent.agent_change_password',compact('profileData'));
+     
+          }
+      /** End of AgentChangePassword method. */
+ 
+     
+    
+    /** Start of AgentUpdatePassword method. */
+    public function AgentUpdatePassword(Request $request){
+
+      // Validation 
+      $request->validate([
+          'old_password' => 'required',
+          'new_password' => 'required|confirmed'
+
+      ]);
+
+      /// Match The Old Password
+
+      if (!Hash::check($request->old_password, auth::user()->password)) {
+
+        $notification = array(
+          'message' => 'Old Password Does not Match!',
+          'alert-type' => 'error'
+      );
+
+      return back()->with($notification);
+      }
+
+      /// Update The New Password 
+
+      User::whereId(auth()->user()->id)->update([
+          'password' => Hash::make($request->new_password)
+
+      ]);
+
+      $notification = array(
+          'message' => 'Password Change Successfully',
+          'alert-type' => 'success'
+      );
+
+      return back()->with($notification); 
+
+  }
+  /** End of AgentUpdatePassword method. */
 
 }
--- routes/web.php
+++ routes/web.php
@@ -59,6 +59,18 @@ Route::middleware(['auth','role:admin'])->group(function(){
 Route::middleware(['auth','role:agent'])->group(function(){
     
     Route::get('/agent/dashboard', [AgentController::class, 'AgentController'])->name('agent.dashboard');
+
+    
+    Route::get('/agent/logout', [AgentController::class, 'AgentLogout'])->name('agent.logout');
+    
+    Route::get('/agent/profile', [AgentController::class, 'AgentProfile'])->name('agent.profile');
+
+    Route::post('/agent/profile/store', [AgentController::class, 'AgentProfileStore'])->name('agent.profile.store');
+
+    Route::get('/agent/change/password', [AgentController::class, 'AgentChangePassword'])->name('agent.change.password');
+
+    Route::post('/agent/update/password', [AgentController::class, 'AgentUpdatePassword'])->name('agent.update.password');
+    
 }); // End Group Agent Middleware
 
 Route::get('/agent/login', [AgentController::class, 'AgentLogin'])->name('agent.login')->middleware(RedirectIfAuthenticated::class);
